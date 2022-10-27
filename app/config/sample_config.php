aec229c1dd54cc450562fa44da83d8e30f72393b
Roland.Ortner <ortner.roland@outlook.at>
2022-10-27 14:02:27 +0200

    config file for connection settings

---
+++ app/config/sample_config.php
@@ -0,0 +1,6 @@
+<?php
+// enter your database connection settings here
+define('DB_NAME', 'YOUR_DATABASE_NAME_HERE');
+define('DB_HOST', 'YOUR_HOST_HERE');
+define('DB_USER', 'YOUR_USER_HERE');
+define('DB_PASS', 'YOUR_PASSWORD_HERE');
