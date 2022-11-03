fbe881120c6525590760d3b5726898f20a34ab93
Roland.Ortner <ortner.roland@outlook.at>
2022-11-03 12:13:05 +0100

    param added to function, input filter for year

--- app/actions/index.php
+++ app/actions/index.php
@@ -3,6 +3,20 @@ require_once __DIR__ . '/../functions/data.php';
 
 $connection = get_db();
 
+// used to highlight the current week
 $current_week = date('W');
 
-$meals = get_meals_from_current_year();
+// filter url param 
+if (isset($_GET['year'])) {
+  $get_year = htmlspecialchars(filter_input(INPUT_GET, 'year', FILTER_SANITIZE_SPECIAL_CHARS));
+}
+
+// if no url param is given, get the current year for the table
+if (!isset($get_year)){
+  $current_year = date('Y');
+  $year = 'sundays_' . $current_year;
+} else {
+  $year = 'sundays_' . $get_year;
+}
+
+$meals = get_meals_from_year($year);
--- app/functions/data.php
+++ app/functions/data.php
@@ -2,17 +2,14 @@
 
 require_once __DIR__ . '/database.php';
 
-function get_meals_from_current_year()
-{
-  $current_year = date('Y');
-  $table_year = 'sundays_' . $current_year;
-  
+function get_meals_from_year(string $year): array
+{ 
   $sql = 'SELECT m.id AS essen_id, m.meal AS mahlzeit, m.grilling AS grillen, m.note AS notiz, m.image_path AS bild,
                  s.id AS day_id, s.kw AS kw, s.sunday_date AS sonntag, s.meal_id
-          FROM ' . $table_year . ' s
+          FROM ' . $year . ' s
           JOIN meals m ON s.meal_id = m.id
           ORDER BY kw ASC';
   $res = get_db()->query($sql);
 
-  return $res->fetchAll();
+  return $res->fetchAll(PDO::FETCH_ASSOC);
 }
