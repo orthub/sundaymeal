9de631f5f7cda0af1a15ea6101b96e830968047a
Roland.Ortner <ortner.roland@outlook.at>
2022-11-03 11:46:47 +0100

    query fix join

--- app/functions/data.php
+++ app/functions/data.php
@@ -7,9 +7,11 @@ function get_meals_from_current_year()
   $current_year = date('Y');
   $table_year = 'sundays_' . $current_year;
   
-  $sql = 'SELECT m.id AS meal_id, m.meal AS mahlzeit, m.grilling AS grillen, m.note AS notiz, m.image_path AS bild,
-                 s.id AS day_id, s.kw AS kw, s.sunday_date AS sonntag, s.meal_id AS essen_id
-          FROM meals m, ' . $table_year . ' s';
+  $sql = 'SELECT m.id AS essen_id, m.meal AS mahlzeit, m.grilling AS grillen, m.note AS notiz, m.image_path AS bild,
+                 s.id AS day_id, s.kw AS kw, s.sunday_date AS sonntag, s.meal_id
+          FROM ' . $table_year . ' s
+          JOIN meals m ON s.meal_id = m.id
+          ORDER BY kw ASC';
   $res = get_db()->query($sql);
 
   return $res->fetchAll();
