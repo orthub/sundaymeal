fb2ded16e60dea37b0cdb02650301a101c648e1c
Roland.Ortner <ortner.roland@outlook.at>
2022-11-09 11:16:04 +0100

    cleanup

--- app/actions/menu.php
+++ app/actions/menu.php
@@ -1,7 +1,5 @@
 <?php
+// get the existing tables for all years
 require_once __DIR__ . '/../functions/menu.php';
 
-$connection = get_db();
-
 $database_years = get_all_sunday_tables();
-str_replace('sundays_', '', $database_years);
