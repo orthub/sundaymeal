09c227555ed754e32d339f4b85c4dffffdf944f5
Roland.Ortner <ortner.roland@outlook.at>
2022-11-09 11:10:34 +0100

    url param for current year

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
 <?php
+$current_year = date('Y');
 require_once __DIR__ . '/app/helpers/session.php';
-header('Location:' . '/public/views/index.php');
+header('Location:' . '/public/views/index.php?year=' . $current_year);
