005ff04a60bda0c0aa6dc5995223d0dcebfed248
Roland.Ortner <ortner.roland@outlook.at>
2022-11-14 17:46:23 +0100

    variable for current year highlighting

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php
 $current_year = date('Y');
 require_once __DIR__ . '/app/helpers/session.php';
-header('Location:' . '/public/views/index.php?year=' . $current_year);
+header('Location:' . '/public/views/index.php?year=' . $current_year . '&current-year=true');
